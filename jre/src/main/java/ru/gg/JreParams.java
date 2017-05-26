a89631189618d60862620d4b9f18bcee5a89e93d
dim <avdim@mail.ru>
2017-05-26 10:17:58 +0300

    native cmd

--- jre/src/main/java/ru/gg/JreParams.java
+++ jre/src/main/java/ru/gg/JreParams.java
@@ -4,7 +4,6 @@ import ru.gg.lib_gwt.JsonBasic;
 
 public class JreParams extends JsonBasic {
 public String statusUrl = "http://narcos-mac.herokuapp.com/status";
-public String bluestacksCmd = "/usr/bin/open Applications/BlueStacks.app/";
 public String adbPath = "/Users/urij/Library/Android/sdk/platform-tools/adb";
 
 }
