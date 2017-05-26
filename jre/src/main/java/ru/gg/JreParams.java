e32568c3d605b04d8efb0309bfb7c19107796ff1
dim <avdim@mail.ru>
2017-05-26 10:14:15 +0300

    open

--- jre/src/main/java/ru/gg/JreParams.java
+++ jre/src/main/java/ru/gg/JreParams.java
@@ -4,7 +4,7 @@ import ru.gg.lib_gwt.JsonBasic;
 
 public class JreParams extends JsonBasic {
 public String statusUrl = "http://narcos-mac.herokuapp.com/status";
-public String bluestacksCmd = "open BlueStacks.app/";
-public String adbPath = "$HOME/Library/Android/sdk/platform-tools/adb";
+public String bluestacksCmd = "/usr/bin/open Applications/BlueStacks.app/";
+public String adbPath = "/Users/urij/Library/Android/sdk/platform-tools/adb";
 
 }
